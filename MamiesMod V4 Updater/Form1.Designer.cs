7befac9d9d8e8df6f83cea917fe90dd4e190ae73
Nicolas Toro <61847756+nt-games-ytb@users.noreply.github.com>
2021-09-01 19:07:03 +0200

    Add files via upload

---
+++ MamiesMod V4 Updater/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace MamiesMod_V4_Updater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.downloadBar = new MetroFramework.Controls.MetroProgressBar();
+            this.downloadText = new System.Windows.Forms.Label();
+            this.downloadName = new System.Windows.Forms.Label();
+            this.updateLabel = new System.Windows.Forms.Label();
+            this.closeLabel = new System.Windows.Forms.Label();
+            this.versionText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // downloadBar
+            // 
+            this.downloadBar.Location = new System.Drawing.Point(25, 150);
+            this.downloadBar.Name = "downloadBar";
+            this.downloadBar.Size = new System.Drawing.Size(500, 23);
+            this.downloadBar.Style = MetroFramework.MetroColorStyle.Magenta;
+            this.downloadBar.TabIndex = 0;
+            this.downloadBar.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // downloadText
+            // 
+            this.downloadText.AutoSize = true;
+            this.downloadText.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.downloadText.ForeColor = System.Drawing.Color.White;
+            this.downloadText.Location = new System.Drawing.Point(150, 90);
+            this.downloadText.Name = "downloadText";
+            this.downloadText.Size = new System.Drawing.Size(196, 31);
+            this.downloadText.TabIndex = 1;
+            this.downloadText.Text = "Download : 0%";
+            // 
+            // downloadName
+            // 
+            this.downloadName.AutoSize = true;
+            this.downloadName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.downloadName.ForeColor = System.Drawing.Color.White;
+            this.downloadName.Location = new System.Drawing.Point(22, 131);
+            this.downloadName.Name = "downloadName";
+            this.downloadName.Size = new System.Drawing.Size(75, 16);
+            this.downloadName.TabIndex = 6;
+            this.downloadName.Text = "Download :";
+            // 
+            // updateLabel
+            // 
+            this.updateLabel.AutoSize = true;
+            this.updateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateLabel.ForeColor = System.Drawing.Color.White;
+            this.updateLabel.Location = new System.Drawing.Point(21, 60);
+            this.updateLabel.Name = "updateLabel";
+            this.updateLabel.Size = new System.Drawing.Size(191, 24);
+            this.updateLabel.TabIndex = 7;
+            this.updateLabel.Text = "Installing the update...";
+            // 
+            // closeLabel
+            // 
+            this.closeLabel.AutoSize = true;
+            this.closeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeLabel.ForeColor = System.Drawing.Color.White;
+            this.closeLabel.Location = new System.Drawing.Point(347, 60);
+            this.closeLabel.Name = "closeLabel";
+            this.closeLabel.Size = new System.Drawing.Size(178, 24);
+            this.closeLabel.TabIndex = 8;
+            this.closeLabel.Text = "Do not close this tab";
+            this.closeLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // versionText
+            // 
+            this.versionText.AutoSize = true;
+            this.versionText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.versionText.ForeColor = System.Drawing.Color.White;
+            this.versionText.Location = new System.Drawing.Point(402, 31);
+            this.versionText.Name = "versionText";
+            this.versionText.Size = new System.Drawing.Size(123, 20);
+            this.versionText.TabIndex = 9;
+            this.versionText.Text = "Version : 4.0.0.0";
+            this.versionText.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 200);
+            this.Controls.Add(this.versionText);
+            this.Controls.Add(this.closeLabel);
+            this.Controls.Add(this.updateLabel);
+            this.Controls.Add(this.downloadName);
+            this.Controls.Add(this.downloadText);
+            this.Controls.Add(this.downloadBar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Magenta;
+            this.Text = "MamiesMod V4 Updater";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroProgressBar downloadBar;
+        private System.Windows.Forms.Label downloadText;
+        private System.Windows.Forms.Label downloadName;
+        private System.Windows.Forms.Label updateLabel;
+        private System.Windows.Forms.Label closeLabel;
+        private System.Windows.Forms.Label versionText;
+    }
+}
+
---
+++ MamiesMod V4 Updater/Form1.cs
@@ -0,0 +1,214 @@
+﻿#region Using
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+#endregion
+using MamiesMod_V4_Updater;
+using MetroFramework;
+using MetroFramework.Forms;
+
+namespace MamiesMod_V4_Updater
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Private
+        WebClient client = new WebClient();
+        WebClient client1 = new WebClient();
+        WebClient client2 = new WebClient();
+        WebClient client3 = new WebClient();
+        WebClient client4 = new WebClient();
+        WebClient client5 = new WebClient();
+        WebClient client6 = new WebClient();
+        WebClient client7 = new WebClient();
+        int fileNumber;
+        string site = "http://nt-games-site.000webhostapp.com/img/MamiesMod%20V4/";
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WebClient webClient = new WebClient();
+            try
+            {
+                versionText.Text = "Version : " + webClient.DownloadString("https://pastebin.com/raw/BMqeJ1b1");
+                Show();
+                Thread.Sleep(5000);
+                update();
+            }
+            catch
+            {
+                MessageBox.Show("You are not connected to internet or the MamiesMod site is down !", "MamiesMod V4 Updater");
+                Close();
+            }
+        }
+
+        private void update()
+        {
+            #region Test
+            /*if (fileNumber == 0)
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadProgressChanged += progressBarNumber;
+                webClient.DownloadFileCompleted += downloadFileCompleted;
+                Uri uri = new Uri("https://nt-games-site.000webhostapp.com/img/update/es/MamiesMod%20V2.resources.dll");
+                webClient.DownloadFileAsync(uri, "en/MamiesMod V4.resources.dll");
+                downloadName.Text = "Download : MamiesMod V4 English langage";
+            }
+            if (fileNumber == 2)
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadProgressChanged += progressBarNumber;
+                webClient.DownloadFileCompleted += downloadFileCompleted;
+                Uri uri = new Uri(site + "https://nt-games-site.000webhostapp.com/img/update/es/MamiesMod%20V2.resources.dll");
+                webClient.DownloadFileAsync(uri, "fr/MamiesMod V4.resources.dll");
+                downloadName.Text = "Download : MamiesMod V4 French langage";
+            }
+            
+                /*if (fileNumber == 2)
+                {
+                    Uri uri = new Uri(site + "ja/MamiesMod%20V4.resources.dll");
+                    webClient.DownloadFileAsync(uri, "ja/MamiesMod V4.resources.dll");
+                    downloadName.Text = "Download : MamiesMod V4 Japnese langage";
+                }
+                if (fileNumber == 3)
+                {
+                    Uri uri = new Uri(site + "de/MamiesMod%20V4.resources.dll");
+                    webClient.DownloadFileAsync(uri, "de/MamiesMod V4.resources.dll");
+                    downloadName.Text = "Download : MamiesMod V4 German langage";
+                }
+                if (fileNumber == 4)
+                {
+                    Uri uri = new Uri(site + "es/MamiesMod%20V4.resources.dll");
+                    webClient.DownloadFileAsync(uri, "es/MamiesMod V4.resources.dll");
+                    downloadName.Text = "Download : MamiesMod V4 Spanish langage";
+                }
+                if (fileNumber == 5)
+                {
+                    Uri uri = new Uri(site + "it/MamiesMod%20V4.resources.dll");
+                    webClient.DownloadFileAsync(uri, "it/MamiesMod V4.resources.dll");
+                    downloadName.Text = "Download : MamiesMod V4 Italian langage";
+                }*/
+
+            /*if (fileNumber == 1)
+            {
+                WebClient webClient1 = new WebClient();
+                webClient1.DownloadProgressChanged += progressBarNumber;
+                webClient1.DownloadFileCompleted += downloadFileCompleted;
+                Uri uri = new Uri(site + "MamiesMod%20V4.exe");
+                webClient1.DownloadFileAsync(uri, "MamiesMod V4.exe");
+                downloadName.Text = "Download : MamiesMod V4";
+            }
+
+            if (fileNumber == 7)
+            {
+                MessageBox.Show("MamiesMod V4 is updated !", "MamiesMod V4 Updater", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }*/
+            #endregion
+
+            client.DownloadProgressChanged += progressBarNumber;
+            client.DownloadFileCompleted += Client_DownloadFileCompleted;
+
+            client1.DownloadProgressChanged += progressBarNumber;
+            client1.DownloadFileCompleted += Client_DownloadFileCompleted1;
+
+            client2.DownloadProgressChanged += progressBarNumber;
+            client2.DownloadFileCompleted += Client_DownloadFileCompleted2;
+
+            client3.DownloadProgressChanged += progressBarNumber;
+            client3.DownloadFileCompleted += Client_DownloadFileCompleted3;
+
+            client4.DownloadProgressChanged += progressBarNumber;
+            client4.DownloadFileCompleted += Client_DownloadFileCompleted4;
+
+            client5.DownloadProgressChanged += progressBarNumber;
+            client5.DownloadFileCompleted += Client_DownloadFileCompleted5;
+
+            client6.DownloadProgressChanged += progressBarNumber;
+            client6.DownloadFileCompleted += Client_DownloadFileCompleted6;
+
+            Uri uri = new Uri(site + "en/MamiesMod%20V4.resources.dll");
+            client.DownloadFileAsync(uri, "en/MamiesMod V4.resources.dll");
+            downloadName.Text = "Download : MamiesMod V4 English langage";
+        }
+
+        private void Client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            Uri uri = new Uri(site + "fr/MamiesMod%20V4.resources.dll");
+            client1.DownloadFileAsync(uri, "fr/MamiesMod V4.resources.dll");
+            downloadName.Text = "Download : MamiesMod V4 French langage";
+        }
+
+        private void Client_DownloadFileCompleted1(object sender, AsyncCompletedEventArgs e)
+        {
+            Uri uri = new Uri(site + "ja/MamiesMod%20V4.resources.dll");
+            client2.DownloadFileAsync(uri, "ja/MamiesMod V4.resources.dll");
+            downloadName.Text = "Download : MamiesMod V4 Japnese langage";
+        }
+
+        private void Client_DownloadFileCompleted2(object sender, AsyncCompletedEventArgs e)
+        {
+            Uri uri = new Uri(site + "de/MamiesMod%20V4.resources.dll");
+            client3.DownloadFileAsync(uri, "de/MamiesMod V4.resources.dll");
+            downloadName.Text = "Download : MamiesMod V4 German langage";
+        }
+
+        private void Client_DownloadFileCompleted3(object sender, AsyncCompletedEventArgs e)
+        {
+            Uri uri = new Uri(site + "es/MamiesMod%20V4.resources.dll");
+            client4.DownloadFileAsync(uri, "es/MamiesMod V4.resources.dll");
+            downloadName.Text = "Download : MamiesMod V4 Spanish langage";
+        }
+
+        private void Client_DownloadFileCompleted4(object sender, AsyncCompletedEventArgs e)
+        {
+            Uri uri = new Uri(site + "it/MamiesMod%20V4.resources.dll");
+            client5.DownloadFileAsync(uri, "it/MamiesMod V4.resources.dll");
+            downloadName.Text = "Download : MamiesMod V4 Italian langage";
+        }
+
+        private void Client_DownloadFileCompleted5(object sender, AsyncCompletedEventArgs e)
+        {
+            Uri uri = new Uri(site + "MamiesMod%20V4.exe");
+            client6.DownloadFileAsync(uri, "MamiesMod V4.exe");
+            downloadName.Text = "Download : MamiesMod V4";
+        }
+
+        private void Client_DownloadFileCompleted6(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("MamiesMod V4 is updated !", "MamiesMod V4 Updater", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
+        }
+
+        private void progressBarNumber(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate ()
+            {
+                downloadBar.Minimum = 0;
+                double receive = double.Parse(e.BytesReceived.ToString());
+                double total = double.Parse(e.TotalBytesToReceive.ToString());
+                double percentage = receive / total * 100;
+                downloadText.Text = $"Download : {string.Format("{0:0.##}", percentage)}%";
+                downloadBar.Value = int.Parse(Math.Truncate(percentage).ToString());
+            }));
+        }
+
+        private void downloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            fileNumber = fileNumber + 1;
+            update();
+        }
+    }
+}
