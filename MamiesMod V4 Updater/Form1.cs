41ed6d1322ecbca4d2f66f560b2c39bc87a3ed90
Nicolas Toro <61847756+nt-games-ytb@users.noreply.github.com>
2021-09-01 19:13:19 +0200

    Update Form1.cs

--- MamiesMod V4 Updater/Form1.cs
+++ MamiesMod V4 Updater/Form1.cs
@@ -56,68 +56,6 @@ namespace MamiesMod_V4_Updater
 
         private void update()
         {
-            #region Test
-            /*if (fileNumber == 0)
-            {
-                WebClient webClient = new WebClient();
-                webClient.DownloadProgressChanged += progressBarNumber;
-                webClient.DownloadFileCompleted += downloadFileCompleted;
-                Uri uri = new Uri("https://nt-games-site.000webhostapp.com/img/update/es/MamiesMod%20V2.resources.dll");
-                webClient.DownloadFileAsync(uri, "en/MamiesMod V4.resources.dll");
-                downloadName.Text = "Download : MamiesMod V4 English langage";
-            }
-            if (fileNumber == 2)
-            {
-                WebClient webClient = new WebClient();
-                webClient.DownloadProgressChanged += progressBarNumber;
-                webClient.DownloadFileCompleted += downloadFileCompleted;
-                Uri uri = new Uri(site + "https://nt-games-site.000webhostapp.com/img/update/es/MamiesMod%20V2.resources.dll");
-                webClient.DownloadFileAsync(uri, "fr/MamiesMod V4.resources.dll");
-                downloadName.Text = "Download : MamiesMod V4 French langage";
-            }
-            
-                /*if (fileNumber == 2)
-                {
-                    Uri uri = new Uri(site + "ja/MamiesMod%20V4.resources.dll");
-                    webClient.DownloadFileAsync(uri, "ja/MamiesMod V4.resources.dll");
-                    downloadName.Text = "Download : MamiesMod V4 Japnese langage";
-                }
-                if (fileNumber == 3)
-                {
-                    Uri uri = new Uri(site + "de/MamiesMod%20V4.resources.dll");
-                    webClient.DownloadFileAsync(uri, "de/MamiesMod V4.resources.dll");
-                    downloadName.Text = "Download : MamiesMod V4 German langage";
-                }
-                if (fileNumber == 4)
-                {
-                    Uri uri = new Uri(site + "es/MamiesMod%20V4.resources.dll");
-                    webClient.DownloadFileAsync(uri, "es/MamiesMod V4.resources.dll");
-                    downloadName.Text = "Download : MamiesMod V4 Spanish langage";
-                }
-                if (fileNumber == 5)
-                {
-                    Uri uri = new Uri(site + "it/MamiesMod%20V4.resources.dll");
-                    webClient.DownloadFileAsync(uri, "it/MamiesMod V4.resources.dll");
-                    downloadName.Text = "Download : MamiesMod V4 Italian langage";
-                }*/
-
-            /*if (fileNumber == 1)
-            {
-                WebClient webClient1 = new WebClient();
-                webClient1.DownloadProgressChanged += progressBarNumber;
-                webClient1.DownloadFileCompleted += downloadFileCompleted;
-                Uri uri = new Uri(site + "MamiesMod%20V4.exe");
-                webClient1.DownloadFileAsync(uri, "MamiesMod V4.exe");
-                downloadName.Text = "Download : MamiesMod V4";
-            }
-
-            if (fileNumber == 7)
-            {
-                MessageBox.Show("MamiesMod V4 is updated !", "MamiesMod V4 Updater", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                this.Close();
-            }*/
-            #endregion
-
             client.DownloadProgressChanged += progressBarNumber;
             client.DownloadFileCompleted += Client_DownloadFileCompleted;
 
